4b2866a39132882315708ae35a6d20e4ff8fc99b
Yash <137068938+YashTirpude@users.noreply.github.com>
2023-10-04 14:36:36 +0530

    a

--- src/App.js
+++ src/App.js
@@ -17,22 +17,22 @@ function App() {
   const [mode, setMode] = useState('light')
   const [text, setText] = useState('dark')
   return (
-   <HashRouter>
+    <BrowserRouter basename='/my-app'>
+     <Navbar/>
    
    
-   <Navbar/>
    
 
    <Routes>
           <Route exact path='/' element={<News text={text} key='general' setProgress={setProgress} pageSize={pageSize} category='general' apikey={apiKey} />} />
-          <Route exact path='/business' element={<News text={text} key='business' setProgress={setProgress} pageSize={pageSize} category='business' apikey={apiKey} />} />
           <Route exact path='/science' element={<News text={text} key='science' setProgress={setProgress} pageSize={pageSize} category='science' apikey={apiKey} />} />
           <Route exact path='/sports' element={<News text={text} key='sports' setProgress={setProgress} pageSize={pageSize} category='sports' apikey={apiKey} />} />
           <Route exact path='/entertainment' element={<News text={text} key='entertainment' setProgress={setProgress} pageSize={pageSize} category='entertainment' apikey={apiKey} />} />
           <Route exact path='/technology' element={<News text={text} key='technology' setProgress={setProgress} pageSize={pageSize} category='technology' apikey={apiKey} />} />
           <Route exact path='/health' element={<News text={text} key='health' setProgress={setProgress} pageSize={pageSize} category='health' apikey={apiKey} />} />
+          <Route exact path='/business' element={<News text={text} key='business' setProgress={setProgress} pageSize={pageSize} category='business' apikey={apiKey} />} />
         </Routes>
-   </HashRouter>
+   </BrowserRouter>
 
 
 
